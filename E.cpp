5b8a542fef02bb29add71c5c9555e4fdf63a11a3
ArpanBareja <153746655+ArpanBareja@users.noreply.github.com>
2025-04-09 16:52:18 +0530

    memoized

--- E.cpp
+++ E.cpp
@@ -1,31 +1,50 @@
 #include <iostream>
 #include <vector>
 #include <iomanip>
-#include <algorithm>
+#include <cstring>
 using namespace std;
 
-int main() {
-    double x, p;
-    cin >> x >> p;
-    double probWin = p / 100.0;
-    double probLose = 1.0 - probWin;
-    const int MAX_BETS = 1000;
-    vector<vector<double>> dp(MAX_BETS + 1, vector<double>(2, 0.0));
-    
-    double maxProfit = 0.0;
-    for (int i = 1; i <= MAX_BETS; i++) {
-        dp[i][0] = probWin * (dp[i - 1][0] + 1.0) 
-                 + probLose * (dp[i - 1][0] - 1.0);
 
-        
-        dp[i][1] = max(
-            dp[i][0],
-            probWin * (dp[i - 1][1] + 1.0) 
-            + probLose * (dp[i - 1][1] - 1.0 * (1.0 - x / 100.0))
-        );
+double dp[1001][2];
+bool visited[1001][2];
 
-        maxProfit = max(maxProfit, dp[i][1]);
+double x, p;
+double winProbab, loseProbab;
+
+double helper(int i, bool insure) {
+    if (i == 0) return 0.0;
+    if (visited[i][insure]) return dp[i][insure];
+    visited[i][insure] = true;
+
+    double result = 0.0;
+
+    if (!insure) {
+        double stayUninsure = winProbab * (helper(i - 1, false) + 1.0) 
+                             + loseProbab * (helper(i - 1, false) - 1.0);
+
+        double startInsurance = winProbab * (helper(i - 1, true) + 1.0)
+                              + loseProbab * (helper(i - 1, true) - (1.0 - x / 100.0));
+
+        result = max(stayUninsure, startInsurance);
+    } else {
+        result = winProbab * (helper(i - 1, true) + 1.0)
+               + loseProbab * (helper(i - 1, true) - (1.0 - x / 100.0));
     }
+
+    return dp[i][insure] = result;
+}
+
+int main() {
+    cin >> x >> p;
+    winProbab = p / 100.0;
+    loseProbab = 1.0 - winProbab;
+
+    memset(visited, false, sizeof(visited));
+
+    double maxProfit = 0.0;
+    for (int i = 1; i <= 1000; ++i) {
+        maxProfit = max(maxProfit, helper(i, false));
+    }
+
     cout << fixed << setprecision(8) << max(0.0, maxProfit) << endl;
-    return 0;
 }
